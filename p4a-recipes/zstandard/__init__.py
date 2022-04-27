7e4eab72da2e888d59fc058f9c36ad636a64e3b8
Dylan McCall <dylan@endlessos.org>
2022-04-27 14:59:27 -0700

    Add kolibri_zim_plugin
    https://phabricator.endlessm.com/T33395

---
+++ p4a-recipes/zstandard/__init__.py
@@ -0,0 +1,19 @@
+from pythonforandroid.recipe import CompiledComponentsPythonRecipe
+
+
+class ZStandardRecipe(CompiledComponentsPythonRecipe):
+    """
+    Recipe for zstandard
+    """
+
+    name = "zstandard"
+    version = "0.18.0"
+    url = (
+        "https://pypi.python.org/packages/source/z/zstandard/zstandard-{version}.tar.gz"
+    )
+    depends = ["cffi", "setuptools"]
+
+    call_hostpython_via_targetpython = False  # For setuptools
+
+
+recipe = ZStandardRecipe()
