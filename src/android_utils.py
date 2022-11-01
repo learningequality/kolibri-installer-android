2cbe022ba1fabe5622743f813674dcc70c7756d3
Manuel Quiñones <manuel.por.aca@gmail.com>
2022-11-01 14:59:17 -0300

    utils: Keep reference to send intent

--- src/android_utils.py
+++ src/android_utils.py
@@ -66,6 +66,7 @@ USB_CONTENT_FLAG_FILENAME = "usb_content_flag"
 _choose_directory_intent = None
 _notification_builder = None
 _notification_intent = None
+_send_intent = None
 
 # Path.is_relative_to only on python 3.9+.
 if not hasattr(Path, "is_relative_to"):
@@ -627,13 +628,14 @@ def send_whatsapp_message(msg):
 
 
 def share_by_intent(path=None, filename=None, message=None, app=None, mimetype=None):
+    global _send_intent
 
     assert (
         path or message or filename
     ), "Must provide either a path, a filename, or a msg to share"
 
-    sendIntent = Intent()
-    sendIntent.setAction(Intent.ACTION_SEND)
+    _send_intent = Intent()
+    _send_intent.setAction(Intent.ACTION_SEND)
     if path:
         uri = FileProvider.getUriForFile(
             Context.getApplicationContext(),
@@ -641,17 +643,18 @@ def share_by_intent(path=None, filename=None, message=None, app=None, mimetype=N
             File(path),
         )
         parcelable = cast("android.os.Parcelable", uri)
-        sendIntent.putExtra(Intent.EXTRA_STREAM, parcelable)
-        sendIntent.setType(AndroidString(mimetype or "*/*"))
-        sendIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        _send_intent.putExtra(Intent.EXTRA_STREAM, parcelable)
+        _send_intent.setType(AndroidString(mimetype or "*/*"))
+        _send_intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
     if message:
         if not path:
-            sendIntent.setType(AndroidString(mimetype or "text/plain"))
-        sendIntent.putExtra(Intent.EXTRA_TEXT, AndroidString(message))
+            _send_intent.setType(AndroidString(mimetype or "text/plain"))
+        _send_intent.putExtra(Intent.EXTRA_TEXT, AndroidString(message))
     if app:
-        sendIntent.setPackage(AndroidString(app))
-    sendIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-    get_activity().startActivity(sendIntent)
+        _send_intent.setPackage(AndroidString(app))
+    _send_intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    get_activity().startActivity(_send_intent)
+    _send_intent = None
 
 
 def make_service_foreground(title, message):
