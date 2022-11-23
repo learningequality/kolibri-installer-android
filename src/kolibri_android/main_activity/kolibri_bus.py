c15c082e6f36f3165ba5db9023b9a8e6fed26c0c
Manuel Quiñones <manuel.por.aca@gmail.com>
2022-11-23 18:06:44 -0300

    Run Kolibri service workers in main activity
    Remove the foreground service which was never shutting down properly.
    Instead import ServicesPlugin from Kolibri and make it a listener of the
    process bus. So that when the process bus stops the workers shutdown
    properly.
    
    https://phabricator.endlessm.com/T34138

--- src/kolibri_android/main_activity/kolibri_bus.py
+++ src/kolibri_android/main_activity/kolibri_bus.py
@@ -1,6 +1,7 @@
 from kolibri.plugins.app.utils import interface
 from kolibri.utils.server import BaseKolibriProcessBus
 from kolibri.utils.server import KolibriServerPlugin
+from kolibri.utils.server import ServicesPlugin
 from kolibri.utils.server import ZeroConfPlugin
 from kolibri.utils.server import ZipContentServerPlugin
 from magicbus.plugins import SimplePlugin
@@ -9,6 +10,8 @@ from magicbus.plugins import SimplePlugin
 class KolibriAppProcessBus(BaseKolibriProcessBus):
     def __init__(self, *args, enable_zeroconf=True, **kwargs):
         super(KolibriAppProcessBus, self).__init__(*args, **kwargs)
+
+        ServicesPlugin(self).subscribe()
 
         if enable_zeroconf:
             ZeroConfPlugin(self, self.port).subscribe()
@@ -33,4 +36,3 @@ class AppPlugin(SimplePlugin):
             "http://127.0.0.1:{port}".format(port=port) + interface.get_initialize_url()
         )
         self.application.load_url(start_url)
-        self.application.start_service("workers")
